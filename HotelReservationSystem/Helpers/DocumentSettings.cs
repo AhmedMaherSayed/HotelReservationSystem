bb3b54bd477e6f5b611fdaf9bff0ab61106133cd
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2025-03-06 19:24:28 +0200

    Add Upload Image

---
+++ HotelReservationSystem/Helpers/DocumentSettings.cs
@@ -0,0 +1,36 @@
+﻿namespace HotelReservationSystem.Helpers
+{
+    public static class DocumentSettings
+    {
+        public static string UploadImage(IFormFile file)
+        {
+            if (file is null)
+                throw new ArgumentNullException("No File is provided for upload.");
+
+            if (!IsImageFile(file))
+                throw new BadImageFormatException("Uploaded file is not image.");
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Images");
+
+            var fileName = $"{Guid.NewGuid()}-{Path.GetFileName(file.FileName)}";
+            var finalPath = Path.Combine(path, fileName);
+
+            using (var fileStream = new FileStream(finalPath, FileMode.Create))
+            {
+                file.CopyTo(fileStream);
+            }
+            return fileName;
+        }
+
+        public static bool IsImageFile(IFormFile file)
+        {
+            if (file is null || file.FileName is null)
+                return false;
+
+            var allowedImageExtenstions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".svg", ".webp" };
+            var fileExtension = Path.GetExtension(file.FileName);
+
+            return allowedImageExtenstions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
