316de2fcc9d7fb1a5537bdf698007433cdd5c461
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2025-03-06 17:25:06 +0200

    Add Exception Error Handling and Transaction Middlewares

---
+++ HotelReservationSystem/Middelwares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using HotelReservationSystem.Data.Enums;
+using HotelReservationSystem.ViewModels;
+
+namespace HotelReservationSystem.Middelwares
+{
+    public class GlobalErrorHandlingMiddleware(ILogger logger) : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                var response = new ResponseViewModel<bool>();
+                await context.Response
+                    .WriteAsJsonAsync(ResponseViewModel<bool>.Failure(ErrorCode.BadRequest, $"{ex.InnerException?.Message ?? ex.Message}"));
+
+                logger.LogError(ex.InnerException?.Message ?? ex.Message);
+                throw;
+            }
+        }
+    }
+}
